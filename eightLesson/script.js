e150664fd698904a9b45bc6cbdb383d59afd32fe
Ihar Vypau <igor.vypov@gmail.com>
2018-06-29 08:14:15 +0300

    Eeight lesson

--- eightLesson/script.js
+++ eightLesson/script.js
@@ -5,7 +5,7 @@ const requestMsg="Ввведите ваше имя";
 const errorMsg="Вы не ввели имя";
 
 const name=prompt(requestMsg);
-if(name.length>=2){
+if(name.length>1){
   alert(name);
 }else{
   alert(errorMsg);
